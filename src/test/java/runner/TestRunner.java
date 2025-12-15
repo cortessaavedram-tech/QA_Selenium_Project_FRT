0cb4849a76d4ec3e258eaf300fe86a8e99b57a88
MªÁngeles Cortés Saavedra <cortessaavedram@gmail.com>
2025-12-15 12:06:59 +0100

    Integración completa con Qase TestOps funcionando

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,9 +6,9 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
+//import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+//import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+//import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
 
 import org.junit.jupiter.api.AfterAll;
 
@@ -18,9 +18,8 @@ import pages.BasePage;
 @IncludeEngines("cucumber") //Cucumber engine activated
 @SelectClasspathResource("features") //It indicates where are the .feature
 @ConfigurationParameters({
-    @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps"), //Package where are the steps
-    @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qase.cucumber7.QaseEventListener"),
-    @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@QaseId=2")
+    @ConfigurationParameter(key = "cucumber.glue", value = "steps"), //Package where are the steps
+    @ConfigurationParameter(key = "cucumber.gilter.tags", value = "@QaseId=2"),
 })
 
 public class TestRunner {
