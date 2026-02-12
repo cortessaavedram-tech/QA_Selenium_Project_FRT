cb88e21dba0924fda894c1c9483a4c5e84436c5f
MªÁngeles Cortés Saavedra <cortessaavedram@gmail.com>
2026-02-12 13:32:31 +0100

    Remove comments runner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,10 +7,6 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-//import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-//import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-//import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
-
 import org.junit.jupiter.api.AfterAll;
 
 import pages.BasePage;
